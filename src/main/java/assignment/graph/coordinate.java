bc96d7cc051cf920f28ffedd9882ea7a7ae6e725
Mohanraj1100 <mohanraj.anbuselvam@tringapps.com>
2023-01-27 17:19:57 +0530

    commit

--- src/main/java/assignment/graph/coordinate.java
+++ src/main/java/assignment/graph/coordinate.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.io.*;
 public class coordinate 
 {
-	 public static void main(String[] args) throws CloneNotSupportedException {
+	 public static void main(String[] args){
 	        Scanner sc = new Scanner(System.in);
 	        System.out.println("Enter x-Coordonate");
 	        Float x = sc.nextFloat();
